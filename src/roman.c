710eebdc66d9c2f60e5c538c510a488b90857f99
Clay Dowling <clay@lazarusid.com>
2017-12-30 12:59:04 -0500

    Recognized the rest of the roman digits

--- src/roman.c
+++ src/roman.c
@@ -8,6 +8,14 @@ static int roman_digit(const char digit) {
             return 5;
         case 'x':
             return 10;
+        case 'l':
+            return 50;
+        case 'c':
+            return 100;
+        case 'd':
+            return 500;
+        case 'm':
+            return 1000;
         default:
             return -1;
     }
--- test/test_roman.c
+++ test/test_roman.c
@@ -7,6 +7,14 @@ void test_vReturns5(void) { TEST_ASSERT_EQUAL_INT(5, roman_value("v")); }
 
 void test_xReturns10(void) { TEST_ASSERT_EQUAL_INT(10, roman_value("x")); }
 
+void test_lReturns50(void) { TEST_ASSERT_EQUAL_INT(50, roman_value("l")); }
+
+void test_cReturns100(void) { TEST_ASSERT_EQUAL_INT(100, roman_value("c")); }
+
+void test_dReturns500(void) { TEST_ASSERT_EQUAL_INT(500, roman_value("d")); }
+
+void test_mReturns1000(void) { TEST_ASSERT_EQUAL_INT(1000, roman_value("m")); }
+
 void test_iiReturns2(void) { TEST_ASSERT_EQUAL_INT(2, roman_value("ii")); }
 
 void test_viReturns6(void) { TEST_ASSERT_EQUAL_INT(6, roman_value("vi")); }
@@ -19,6 +27,10 @@ void roman_suite(void) {
     RUN_TEST(test_iReturns1);
     RUN_TEST(test_vReturns5);
     RUN_TEST(test_xReturns10);
+    RUN_TEST(test_lReturns50);
+    RUN_TEST(test_cReturns100);
+    RUN_TEST(test_dReturns500);
+    RUN_TEST(test_mReturns1000);
     RUN_TEST(test_iiReturns2);
     RUN_TEST(test_viReturns6);
     RUN_TEST(test_ivReturns4);
